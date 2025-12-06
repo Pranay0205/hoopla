fea6246094e709b9acfa0b82a0d410f229bef113
Pranay0205 <pranayghuge@gmail.com>
2025-12-05 23:18:41 -0500

    feat: enhance MultiModalSearch with image embedding and search functionality

--- cli/lib/multimodal_search.py
+++ cli/lib/multimodal_search.py
@@ -1,19 +1,43 @@
+from unittest import result
 from PIL import Image
 from sentence_transformers import SentenceTransformer
-from transformers import AutoModel
+
+from lib.utils.constants import DEFAULT_SEARCH_LIMIT
+from lib.utils.math_utils import cosine_similarity
+from lib.utils.search_utils import format_search_result, load_movies
 
 
 class MultiModalSearch:
 
-    def __init__(self, model_name="clip-ViT-B-32"):
+    def __init__(self, model_name="clip-ViT-B-32", document=[]):
         self.model_name = model_name
-        self.model = SentenceTransformer(self.model_name)
+        self.model = SentenceTransformer(
+            self.model_name, model_kwargs={'use_fast': False})
+        self.document = document
+        self.texts = [
+            f"{doc['title']}: {doc['description']}" for doc in self.document]
+        self.text_embeddings = self.model.encode(
+            self.texts, show_progress_bar=True)
 
     def embed_image(self, image_path: str):
         image = Image.open(image_path)
-        image_embeddings = self.model.encode([image])
+        image_embeddings = self.model.encode([image], show_progress_bar=True)
 
-        return image_embeddings[0]  # Return first element if it's a list
+        return image_embeddings[0]
+
+    def search_with_image(self, image_path: str) -> list[dict]:
+        image_embedding = self.embed_image(image_path)
+
+        result = []
+        for i, embedding in enumerate(self.text_embeddings):
+            similarity = cosine_similarity(embedding, image_embedding)
+
+            result.append(format_search_result(
+                self.document[i]["id"], self.document[i]["title"], score=similarity, document=self.document[i]["description"]))
+
+        result.sort(key=lambda x: x["score"], reverse=True)
+
+        return result[:DEFAULT_SEARCH_LIMIT]
 
 
 def verify_image_embedding(image_path: str):
@@ -22,3 +46,14 @@ def verify_image_embedding(image_path: str):
     image_embedding = model.embed_image(image_path)
 
     return image_embedding
+
+
+def image_search_command(image_path: str) -> list[dict]:
+
+    documents = load_movies()
+
+    model = MultiModalSearch(document=documents)
+
+    results = model.search_with_image(image_path)
+
+    return results
--- cli/multimodal_search_cli.py
+++ cli/multimodal_search_cli.py
@@ -1,7 +1,8 @@
 import argparse
+import enum
 import os
 
-from lib.multimodal_search import verify_image_embedding
+from lib.multimodal_search import image_search_command, verify_image_embedding
 from lib.utils.constants import PROJECT_ROOT
 
 
@@ -17,6 +18,11 @@ def main() -> None:
     verify_image_embedding_parser.add_argument(
         "image_path", type=str, help="Path of the image")
 
+    image_search = subparsers.add_parser(
+        "image_search", help="search with image or description")
+
+    image_search.add_argument("image_path", type=str, help="Path of the image")
+
     args = parser.parse_args()
 
     match args.command:
@@ -25,7 +31,7 @@ def main() -> None:
 
         case "verify_image_embedding":
 
-            print("executing command verify image embedding")
+            print("executing command verify image embedding...")
 
             if args.image_path == "":
                 raise ValueError("path cannot be empty")
@@ -34,10 +40,29 @@ def main() -> None:
 
             image_embeddings = verify_image_embedding(image_path)
 
-            if not image_embeddings:
+            if image_embeddings is None or image_embeddings.size == 0:
                 raise ValueError("no embeddings generated")
 
             print(f"Embedding shape: {image_embeddings.shape[0]} dimensions")
+
+        case "image_search":
+
+            print("excuting command for image search...")
+
+            if args.image_path == "":
+                raise ValueError("path cannot be empty")
+
+            image_path = os.path.join(PROJECT_ROOT, args.image_path)
+
+            results = image_search_command(image_path)
+
+            if results is None or len(results) == 0:
+                raise ValueError("no results found")
+
+            for index, result in enumerate(results, 1):
+                print(
+                    f"{index}. {result["title"]} (similarity: {result["score"]:.3f})")
+                print(f"  {result["document"]}...")
 
 
 if __name__ == "__main__":
